7dab8413a534a84267052b6c64394f17f505657c
dyari <01di4r@gmail.com>
2025-02-19 22:53:32 +0300

    Rx is easy

--- Nivl/Presentation/Home/HomeViewController.swift
+++ Nivl/Presentation/Home/HomeViewController.swift
@@ -16,10 +16,12 @@ class HomeViewController: UIViewController, Storyboarded {
     private let viewModel = HomeViewModel()
     private let disposeBag = DisposeBag()
     
+    private let throttleIntervalInMilliseconds = 100_000_0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        viewModel.search(query: "Supernova")
+        setupSearchObserver()
         setupCollectionView()
     }
     
@@ -32,6 +34,17 @@ class HomeViewController: UIViewController, Storyboarded {
                     }
                 }
                 .disposed(by: disposeBag)
+    }
+    
+    func setupSearchObserver() {
+        navigationItem.searchController?.searchBar.rx.text
+            .observe(on: MainScheduler.asyncInstance)
+            .distinctUntilChanged()
+            .throttle(.microseconds(throttleIntervalInMilliseconds), scheduler: MainScheduler.instance)
+            .subscribe(onNext: { [unowned self] text in
+                viewModel.search(query: (text?.isEmpty == false) ? text! : "Supernova")
+            })
+            .disposed(by: disposeBag)
     }
     
 }
